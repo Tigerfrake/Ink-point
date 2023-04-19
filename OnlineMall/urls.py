507b40c950cd8611477a8a64d7692d9881d5d75a
Brightone <brightoneotis1@gmail.com>
2023-04-19 12:27:26 +0300

    comment added

--- OnlineMall/urls.py
+++ OnlineMall/urls.py
@@ -20,6 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # urlpatterns for products
     path('', include('products.urls')),
     path('reviews/', include('reviews.urls')),
     path('users/', include('users.urls')),
